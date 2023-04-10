0983f970c33c1be55c9a7f3c26a4198f23349922
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-04-10 12:00:02 +0530

    Add files via upload

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home'),
-    path('prime',views.prime,name='prime')
+    path('prime',views.prime,name='prime'),
+    path('fibonacci',views.fibonacci,name='fibonacci')
 ]
--- studentapp/views.py
+++ studentapp/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    a,b=100,2
-    c=a+b
-    return HttpResponse("result is "+str(c))
+    if request.method=="POST":
+            a = request.POST["txtnum1"]
+            b = request.POST["txtnum2"]
+            c = int(a)+int(b)
+            return HttpResponse("Result is "+str(c))
+    else:         
+      return render(request,"addition.html")
 def prime(request):
-    num=6
-    s=''
-    for i in range(2,num):
+    if request.method=="POST":
+      num=int(request.POST["num"])
+      s=''
+      for i in range(2,num):
         if num%i==0:
             s='not prime'
             break
-    else:
+      else:
         s='prime'
-    return HttpResponse(s)           
+      return HttpResponse(s) 
+    else:
+        return render(request,"prime.html")            
+
+def fibonacci(request):
+    a=-1
+    b=1
+    res=''
+    for i in range(10):
+        c=a+b
+        res+=str(c) + " "
+        a=b
+        b=c
+    return HttpResponse(res)    
 
